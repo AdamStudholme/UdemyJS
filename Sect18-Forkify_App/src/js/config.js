2a0e7044930b85a1605c085222f1592cc62594d7
Adam Studholme <adam.studholme@version1.com>
2024-03-26 14:09:07 +0000

    config and helper files added

---
+++ Sect18-Forkify_App/src/js/config.js
@@ -0,0 +1,5 @@
+//File that contains all the global variables for the project are required throughout the project
+
+//recipe API url root
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
