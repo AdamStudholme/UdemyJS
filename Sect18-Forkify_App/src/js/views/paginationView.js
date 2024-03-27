3f8e1110af3539097815b1d1a8360247f321101c
Adam Studholme <adam.studholme@version1.com>
2024-03-27 10:49:18 +0000

    refactored paginationView removed repeat markup

--- Sect18-Forkify_App/src/js/views/paginationView.js
+++ Sect18-Forkify_App/src/js/views/paginationView.js
@@ -24,51 +24,23 @@ class paginationView extends View {
     );
     //Pag1 and other pages
     if (currPage === 1 && numPages > 1) {
-      return `<button data-goto="${
-        currPage + 1
-      }" class="btn--inline pagination__btn--next">
-      <span>Page ${currPage + 1}</span>
-      <svg class="search__icon">
-        <use href="${icons}#icon-arrow-right"></use>
-      </svg>
-    </button>`;
+      return this._generateMarkupPreview(currPage + 1, 'right');
     }
     //Last page
     if (currPage === numPages && numPages > 1) {
-      return `<button data-goto="${
-        currPage - 1
-      }" class="btn--inline pagination__btn--prev">
-      <svg class="search__icon">
-        <use href="${icons}#icon-arrow-left"></use>
-      </svg>
-      <span>Page ${currPage - 1}</span>
-    </button>`;
+      return this._generateMarkupPreview(currPage - 1, 'left');
     }
     //Other page
     if (currPage < numPages) {
-      return `<button data-goto="${
-        currPage - 1
-      }" class="btn--inline pagination__btn--prev">
-      <svg class="search__icon">
-        <use href="${icons}#icon-arrow-left"></use>
-      </svg>
-      <span>Page ${currPage - 1}</span>
-    </button>
-    <button data-goto="${
-      currPage + 1
-    }" class="btn--inline pagination__btn--next">
-      <span>Page ${currPage + 1}</span>
-      <svg class="search__icon">
-        <use href="${icons}#icon-arrow-right"></use>
-      </svg>
-    </button>`;
+      return (
+        this._generateMarkupPreview(currPage - 1, 'left') +
+        this._generateMarkupPreview(currPage + 1, 'right')
+      );
     }
-    //Page 1 no other pages
-    return '';
   }
 
   _generateMarkupPreview(page, direction) {
-    const nextOrPrev = direction === left ? 'prev' : 'next';
+    const nextOrPrev = direction === 'left' ? 'prev' : 'next';
     return `<button data-goto="${page}" class="btn--inline pagination__btn--${nextOrPrev}">
       <span>Page ${page}</span>
       <svg class="search__icon">
